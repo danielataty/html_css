9d252f596f95002ad5a5321f32753c4bb955e800
Daniela Tati <141074542+danielatati@users.noreply.github.com>
2023-10-04 01:01:08 +0100

    Vários novos exercícios

---
+++ Projetos meus/Calculadora/script.js
@@ -0,0 +1,26 @@
+function soma(x,y){
+    return x + y
+}
+
+function sub(x,y){
+return x - y
+}
+
+function div(x,y){
+    if(y == 0){
+        return "Erro: divisão por zero"
+    }
+    return x / y
+    }
+
+function mult(x,y){
+  return x * y
+}
+
+console.log(soma(9,9))
+
+
+
+
+
+
